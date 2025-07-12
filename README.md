30df235c75f57c79bb9d9ce6473c04ffe6e7c09e
IonAIr1 <ionair.ironwood@gmail.com>
2025-07-12 23:56:50 +0800

    added main function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # seperateprint
+
+Project for personal use. Seperate a pdf file into two files for printing. One with only the colored pixels and the other with only the grayscale pixels.
--- seperateprint.py
+++ seperateprint.py
@@ -1,13 +1,62 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 import pymupdf
 
 
-def image_to_pdf(file_path, output_path, dpi=300):
+
+#convert pdf file to image object
+def image_to_pdf(file_path, dpi=300):
     zoom = dpi / 72  # zoom factor, standard: 72 dpi
     magnify = pymupdf.Matrix(zoom, zoom)
     doc = pymupdf.open(file_path)
+
+    pages = []
     for page in doc:
         pix = page.get_pixmap(matrix=magnify)
-        pix.save(output_path)
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        pages.append(img)
 
-image_to_pdf('input.pdf', 'temp.png')
+    return pages
+
+
+#takes an image and a limit value to separate colored pixels from gray pixels.
+def seperate_page_color(image, limit=10):
+    color_image = image.copy()
+    gray_image = image.copy()
+
+    image_data = image.load()
+    color_image_data = color_image.load()
+    gray_image_data = gray_image.load()
+    height,width = image.size
+
+    #iterate through each pixel
+    for pixely in range(height):
+        for pixelx in range(width):
+            r,g,b = image_data[pixely, pixelx]
+
+            #remove colored pixels from gray image
+            if (abs(r-g) >limit) or (abs(r-b) > limit) or (abs(g-b) > limit):
+                gray_image_data[pixely, pixelx] = 255,255,255
+            
+            #remove gray pixels from color image
+            else:
+                color_image_data[pixely, pixelx] = 255,255,255
+    gray_image = gray_image.convert("L")
+    return gray_image, color_image
+
+
+#Function to separate colored and gray pages from a PDF file
+def seperate(input_path, limit=10):
+    original_pages = image_to_pdf(input_path)
+    color_pages = []
+    gray_pages = []
+    for page in original_pages:
+        gray_image, color_image = seperate_page_color(page, limit=limit)
+        color_pages.append(color_image)
+        gray_pages.append(gray_image)
+
+    color_pages[0].save(input_path.replace(".pdf", "_color.pdf"), save_all=True, append_images=color_pages[1:])
+    gray_pages[0].save(input_path.replace(".pdf", "_gray.pdf"), save_all=True, append_images=gray_pages[1:])
+
+
+
+# seperate("aaa.pdf", limit=10)
